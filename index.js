ca2de0ede057fb4eb93278427f04be2ce71be598
KEKEbl <166295486+KEKEbl@users.noreply.github.com>
2024-04-07 13:29:20 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Partner Yapıyor!","Anime İzliyor"];
 
 
 let currentIndex = 0;
